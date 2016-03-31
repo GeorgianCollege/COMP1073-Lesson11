b5b387a0317526ed12fa0eaed6e054fe81a0de1a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-03-31 11:59:41 -0400

    updated projects page added contact page

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,55 +5,23 @@
 
     "use strict";
 
-    var mainContentH1 = $('#mainContent h1')[0];
-
-    var welcome = $('#welcome').text();
-
-    var mainContentH1JS = document.querySelectorAll('#mainContent h1')[1];
-
-    // console.log(mainContentH1.textContent);
-
-    // console.log(welcome);
-
-    //console.log(mainContentH1JS);
-
-    /*
-    var form1JS = document.getElementById('form1');
-    form1JS.style.width = "50%";
-    */
-
-    $('#form1').css('width', '50%');
-
-    $('#form1 input').attr('placeholder', 'Enter your username');
-
-    //JQUERY VERSION
-    $('#submitButton').mouseover(function() {
-        $('#submitButton').attr('class', 'btn btn-danger');
-    });
-
-    $('#submitButton').mouseout(function() {
-        $('#submitButton').attr('class', 'btn btn-primary');
-    });
-
-
-    $('#defaultButton').click(function() {
-        $('#username').val("Tom's luck number is: " + (Math.floor(Math.random() * 49) + 1));
-    });
-
-    $('#toggleButton').click(function() {
-        if ($('#toggleButton').text() === 'Hide') {
-            $('#form1').hide('1000');
-            $('#toggleButton').text('Show');
-        } else {
-            $('#form1').show('1000');
-            $('#toggleButton').text('Hide');
+    var header = $('#mainHeader').load("partials/nav.html", function(){
+        var bodyid = $('body').attr('id');
+        switch (bodyid.toString()) {
+            case "index":
+                $('#indexLink').attr("class","active");
+                break;
+            case "projects":
+                $('#projectsLink').attr("class","active");
+                break;
+            case "contact":
+                $('#contactLink').attr("class","active");
+                break;
         }
+        
+        
     });
 
-    $('#username').click(function() {
-        $('#username').select();
-    });
-
-
+    
 })();
 
